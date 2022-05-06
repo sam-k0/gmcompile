4907eb8e4471d84d227cef107148bf6880dcc508
Samuel Koob <56673835+sam-k0@users.noreply.github.com>
2022-05-06 21:27:39 +0200

    Initial D

---
+++ README.md
@@ -0,0 +1,24 @@
+# gmcompile README
+
+
+## Features
+
+This extension allows you to compile your GMS1.4 games right away from VSCode!
+
+**Make sure the GMS1.4 IDE is running in the background with your project loaded.**
+
+
+
+## Requirements
+
+If you cannot use this extension right away, you need to install the `ExternalCompiler.exe` from my **GitHub page** [here](https://github.com/sam-k0/quick-compile/releases/tag/1.0).
+
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `myExtension.compilerpath`: The location of "ExternalCompiler.exe"
+
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,48 @@
+
+const { exec } = require('child_process');
+const { toEditorSettings } = require('typescript');
+const vscode = require('vscode');
+const childproc = require('child_process').execFile;
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	//var compilerPath = vscode.workspace.getConfiguration('compilegml').get('compilerpath', __dirname.toString() + "\\ExternalCompiler.exe");
+	//console.log(compilerPath);
+
+	
+
+	console.log('Congratulations, your extension "gmcompile" is now active!');
+	let disposable = vscode.commands.registerCommand('gmcompile.compileGML', function () {
+
+		// Get the compilerpath
+		var compilerPath = vscode.workspace.getConfiguration('compilegml').get('compilerpath');
+		if(compilerPath == null)
+		{
+			compilerPath =  __dirname.toString() + "\\ExternalCompiler.exe";
+		}
+		console.log(compilerPath);
+
+		vscode.window.showInformationMessage("Set compiler path: "+compilerPath+"\nCan be changed in settings.");
+		//"E:\\Modding\\ExternalCompiler.exe"
+		try {
+			exec(compilerPath, function(err,data){
+				console.log(err);
+				console.log(data.toString());
+			});
+		} catch (error) {
+			vscode.window.showInformationMessage("Could not find the ExternalCompiler.exe.\nMake sure to set the path in settings.")
+		}
+		
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
